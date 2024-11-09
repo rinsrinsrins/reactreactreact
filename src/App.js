1b1f276db0d28b8c62626a052d59792a57ef1554
Clarence Prades <clarenceprades.work@gmail.com>
2024-11-09 21:19:49 +0800

    TicTacToe Game

--- src/App.js
+++ src/App.js
@@ -1,77 +1,86 @@
-import './App.css';
+
 import {useState} from 'react';
 
-const user = {
-  name: 'Clarence Prades',
-  age: 22,
-  hobby: "exploring new things"
-};
-
-const likes = [
-  { title: 'Play Roblox', isFav: false, id: 1 },
-  { title: 'Sleep', isFav: false, id: 2 },
-  { title: 'Work', isFav: false, id: 3 },
-  { title: 'Code Code Code', isFav: true, id: 4 },
-];
-
-
-export default function HelloWorld() {
-  const { name, age, hobby } = user;
-  const isClose = true;
-  const textContent = isClose ? "Hello!" : "Hi!";
-
-  const listLikes = likes.map(like => (
-    <li key={like.id} className={like.isFav ? 'fav' : 'regular'}>
-      {like.title}
-    </li>
-  ));
-
-function handleClick() {
-    alert('Why you clicked me?');
-}
-
-const [count, setCount] = useState(0);
-
-
-function addCount(){
-  setCount(count + 1);
-}
-
-function subCount(){
-  setCount(count - 1);
-}
-
-function CountMeAdd(){
-  return(
-    <button onClick={addCount}>
-    Incremented by {count} times.
+function Square({value, onSquareClick}){
+  return (
+    <button 
+    className="square" 
+    onClick={onSquareClick}>
+      {value}
     </button>
-  );     
+  );
 }
 
-function CountMeSub(){
-  return(
-    <button onClick={subCount}>
-    Decremented by {count} times.
-    </button>
-  );     
-}
 
+export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleSquareClick(i) {
+
+    if(squares[i] || calculateWinner(squares)){
+      return;
+    }
+    const nextSquares = squares.slice();
+
+    if(xIsNext){
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+    console.log(`Clicked ${nextSquares[i]} in the board with an index of ${i}`);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner){
+    status = "Winner: " + winner;
+    console.log(`${winner} is winner!`)
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "O" );
+  }
 
   return (
-    <>
-      <h1>Hello, {name}.</h1>
-      <p>I know you are {age} years old.</p>
-      <p>Your hobby is {hobby}.</p>
-      <p>{textContent}</p>
-      <h2>Your likes:</h2>
-      <ul>{listLikes}</ul>
-      <button onClick={handleClick}>
-      Click me!
-      </button>
-      <CountMeAdd />
-      <CountMeAdd />
-      <CountMeSub />
-    </>
+   <>
+   <div className="status">{status}</div>
+   <div className="board-row">
+    <Square value={squares[0]} onSquareClick={()=>handleSquareClick(0)} />
+    <Square value={squares[1]} onSquareClick={()=>handleSquareClick(1)} />
+    <Square value={squares[2]} onSquareClick={()=>handleSquareClick(2)} />
+   </div>
+   <div className="board-row">
+    <Square value={squares[3]} onSquareClick={()=>handleSquareClick(3)} />
+    <Square value={squares[4]} onSquareClick={()=>handleSquareClick(4)} />
+    <Square value={squares[5]} onSquareClick={()=>handleSquareClick(5)} />
+   </div>
+   <div className="board-row">
+    <Square value={squares[6]} onSquareClick={()=>handleSquareClick(6)} />
+    <Square value={squares[7]} onSquareClick={()=>handleSquareClick(7)} />
+    <Square value={squares[8]} onSquareClick={()=>handleSquareClick(8)} />
+   </div>
+   </>
   );
+}
+
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+
+  for (let i = 0; i<lines.length; i++) {
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
